a617e4a8cc180bf6a5b7a7c3ee7ccf8ae6d58d4b
EdwinPinto103 <edwinpinto103@hotmail.com>
2025-06-03 21:22:17 -0500

    Fix visual: importando estilos Tailwind en main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import './index.css'; // Estilos de Tailwind
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
