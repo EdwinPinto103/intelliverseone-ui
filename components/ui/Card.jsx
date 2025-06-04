356aa4e034bd02c300342241567e5ec0de108464
EdwinPinto103 <edwinpinto103@hotmail.com>
2025-06-03 21:11:51 -0500

    Fix: exportando correctamente Card y CardContent

--- components/ui/Card.jsx
+++ components/ui/Card.jsx
@@ -1,3 +1,13 @@
+import React from "react";
+
 export function Card({ children }) {
-  return <div className="card">{children}</div>;
+  return (
+    <div className="bg-white bg-opacity-10 rounded-lg shadow-md p-4">
+      {children}
+    </div>
+  );
+}
+
+export function CardContent({ children }) {
+  return <div className="mt-2">{children}</div>;
 }
